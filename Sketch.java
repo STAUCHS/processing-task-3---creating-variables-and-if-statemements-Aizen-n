4775461f7d3974c2d92afd6945af91bc3be959bd
Aizen-n <javin.thomas24@ycdsbk12.ca>
2024-03-08 13:10:11 -0500

    processing1

--- Sketch.java
+++ Sketch.java
@@ -1,7 +1,15 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
+
+  // Generates a random number from -190 and 100 and -150 and 120
+
+  float circleX = random(-190,100);
+  float circleY = random(-150,120);
+
+  // 
+
+  String currentTime;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -11,26 +19,73 @@ public class Sketch extends PApplet {
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  // Defaultbackground colour
+
   public void setup() {
     background(210, 255, 173);
-  }
 
-  /**
+
+  }
+public void draw() {
+
+    // checks if the circle will be shown on the top or bottom half of the screen 
+     
+
+    double greaterY;
+    greaterY = 150 - (circleY);
+
+    boolean isGreaterY = greaterY > 200;
+
+    // checks if the circle will be shown on the left or right side of the screen 
+
+    double greaterX;
+    greaterX = 150 - (circleX);
+
+    boolean isGreaterX = greaterX > 200;
+
+    boolean isGreaterXY = isGreaterX && isGreaterY;
+
+    // if the circle appears in the bottom-right side of the screen then the background changes to red 
+
+    if (isGreaterXY) {
+
+      background(255,60,0);
+    }
+
+     /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // each x and y cordinates of each shape is either subtracted or added with the randomly generated mumber 
+
+   
+  fill(228,161,62); 
+  ellipse(150 - (circleX),150 - (circleY),200,200);
+
+  fill(204,102,0);
+  ellipse(135 - (circleX),150- (circleY),20,20);
+
+  fill(204,102,0);
+  ellipse(165 - (circleX),150- (circleY),20,20);
+
+
+  rect(120 - (circleX),165- (circleY),60,10);
+
+
+  // shows current time in 24 hr format 
+
+
+  fill(0);
+  textSize(34);
+  currentTime = hour() + ":" + minute();
+  text(currentTime, 150, 50);
+
+
+
+
+
+   
   }
   
-  // define other methods down here.
+  
 }
